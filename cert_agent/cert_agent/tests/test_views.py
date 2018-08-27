bfecbf78b129567054c4ec2319ac12e13cf2be4e
Anders Pearson <anders@thraxil.org>
2018-08-27 15:21:09 +0100

    flesh out the tests
    This should cover all the branches through the views.

--- cert_agent/cert_agent/tests/test_views.py
+++ cert_agent/cert_agent/tests/test_views.py
@@ -1,13 +1,37 @@
-from django.test import TestCase
-from django.test.client import Client
+from django.test import RequestFactory, TestCase
+from ..views import DomainActivateView
 
 
-class IndexTest(TestCase):
-    def test_index(self):
-        """ dummy test to get things started
+class DomainActivateViewTests(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
 
-        the index page just serves up a 404
-        """
-        c = Client()
-        response = c.get("/")
-        self.assertEqual(response.status_code, 404)
+    def test_missing_api_key(self):
+        request = self.factory.post('/domain_activate/')
+        response = DomainActivateView.as_view()(request)
+        self.assertEqual(response.status_code, 403)
+
+    def test_missing_domain(self):
+        request = self.factory.post('/domain_activate/', HTTP_API_KEY='secret_key')
+        response = DomainActivateView.as_view()(request)
+        self.assertEqual(response.status_code, 400)
+
+    def test_invalid_domain(self):
+        request = self.factory.post('/domain_activate/', HTTP_API_KEY='secret_key',
+                                    data={'domain': '!!! '})
+        response = DomainActivateView.as_view()(request)
+        self.assertEqual(response.status_code, 400)
+
+    def test_simple_command(self):
+        request = self.factory.post('/domain_activate/', HTTP_API_KEY='secret_key',
+                                    data={'domain': "www.example.com"})
+        with self.settings(ANSIBLE_CMD="true"):
+            response = DomainActivateView.as_view()(request)
+            self.assertEqual(response.status_code, 202)
+
+    def test_failed_command(self):
+        request = self.factory.post('/domain_activate/', HTTP_API_KEY='secret_key',
+                                    data={'domain': "www.example.com"})
+        with self.settings(ANSIBLE_CMD="false"):
+            response = DomainActivateView.as_view()(request)
+            self.assertEqual(response.status_code, 500)
