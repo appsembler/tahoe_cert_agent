45044b7cb7eeec9234840fdedea02c683560fd19
Anders Pearson <anders@thraxil.org>
2020-06-04 13:04:57 +0100

    Update cert_agent/cert_agent/gunicorn_conf.py
    Co-authored-by: Omar Al-Ithawi <i@omardo.com>

--- cert_agent/cert_agent/gunicorn_conf.py
+++ cert_agent/cert_agent/gunicorn_conf.py
@@ -5,7 +5,8 @@ import beeline
 
 def post_worker_init(worker):
     logging.info(f'beeline initialization in process pid {os.getpid()}')
-    beeline.init(
-        writekey=os.environ.get('HONEYCOMB_WRITEKEY'),
-        dataset=os.environ.get('HONEYCOMB_DATASET'),
-        service_name='cert_agent')
+if os.environ.get('HONEYCOMB_WRITEKEY'):
+        beeline.init(
+            writekey=os.environ['HONEYCOMB_WRITEKEY'],
+            dataset=os.environ['HONEYCOMB_DATASET'],
+            service_name='cert_agent')
